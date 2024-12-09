d39a6372d083b0c0433ade3b41d9528abd779dd3
EllenLitt <t2liel00@students.oamk.fi>
2024-12-09 21:52:34 +0200

    diskografian kuvien koot muutettu

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.getElementById('nav');
-    const intro = document.getElementById('intro');
-
-    function toggleNavVisibility() {
-        const introRect = intro.getBoundingClientRect();
-        if (introRect.top < window.innerHeight && introRect.bottom > 0) {
-            nav.style.display = 'flex'; s
-        } else {
-            nav.style.display = 'none'; 
-        }
-    }
-
- 
-    window.addEventListener('scroll', toggleNavVisibility);
-    window.addEventListener('resize', toggleNavVisibility);
-
-    toggleNavVisibility();
-});
+window.onload = function() {
+    // Aseta 1-2 sekunnin viive logon poistamiseen
+    setTimeout(function() {
+        // Poista logo ja näytä sisältö
+        document.getElementById("logo-container").style.opacity = 0;
+        document.getElementById("content").style.display = "block";
+        
+        // Lisää animaatio sisällön esiin tulemiseksi
+        setTimeout(function() {
+            document.getElementById("content").classList.add("fade-in");
+        }, 100); // Pieni viive ennen animaation alkua
+    }, 1500); // Logo näkyy 1,5 sekuntia ennen kuin se katoaa
+};
